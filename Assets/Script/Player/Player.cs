e6d830a6b7ce72d74bc17e2fcd5ac88610316676
guni0527 <kkh02527@naver.com>
2025-05-21 20:35:30 +0900

    a
    a

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,10 +1,12 @@
-using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
+
+    public ItemData itemData;
+    public Action addItem;
 
     private void Awake()
     {
